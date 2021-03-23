5b9be3b5bfcf7749ed4f621e1193a86b65ffb8df
Shubham510 <Shubham510@users.noreply.github.com>
2021-03-23 12:38:16 +0530

    Fix all_reports tests

--- test/patients_all_reports_api.test.js
+++ test/patients_all_reports_api.test.js
@@ -8,29 +8,26 @@ chai.use(chaiHttp);
 
 
 
-describe("/patient/all_reports", () => {
+describe("/patients/:id/all_reports", () => {
     it("should return all the reports for the specified patient", () => {
         let patient_id = "603ff2867146f371b886d17f";
         chai.request(server)
-            .post('/api/patients/'+ patient_id +'/create_report')
+            .get('/api/patients/'+ patient_id +'/all_reports')
             .set("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDNmZjNlMDcxNDZmMzcxYjg4NmQxODIiLCJpYXQiOjE2MTQ4MDM5NTZ9.vAOhAgaT9dVoY1CEumDFx3HqTT6nV9SWVd2T8UAXdw4")
             .end((err,res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
                 res.body.should.have.property('message').eql("Reports fetched successfully!");
                 res.body.data.should.have.property('all_reports');
-                res.body.data.all_reports.should.have.property("reports").be.a(Array);
+                res.body.data.all_reports.should.have.property("reports").be.a('array');
                 // res.body.data.all_reports.should.have.property("status").eql(report.status);
             });
     });
 
     it("should return error for an invalid patient", () => {
-        let report = {
-            "status":"Travelled-Quarantine"
-        }
         let patient_id = "603ff2867146f371b874d17f";
         chai.request(server)
-            .post('/api/patients/'+patient_id+'/create_report')
+            .get('/api/patients/'+patient_id+'/all_reports')
             .set("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDNmZjNlMDcxNDZmMzcxYjg4NmQxODIiLCJpYXQiOjE2MTQ4MDM5NTZ9.vAOhAgaT9dVoY1CEumDFx3HqTT6nV9SWVd2T8UAXdw4")
             .end((err,res) => {
                 res.should.have.status(404);
